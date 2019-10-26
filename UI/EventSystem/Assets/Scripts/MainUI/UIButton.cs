6afbb4496f453dbf2fa844bb0c3dbfc01ce9e38d
九艺 <artistninth@gmail.com>
2019-10-26 16:48:28 +0800

    UI消息分发框架
    采用delegate实现

---
+++ UI/EventSystem/Assets/Scripts/MainUI/UIButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIButton : MonoBehaviour
+{
+    private Button btnChangeName;
+
+    void Awake()
+    {
+        btnChangeName = GetComponent<Button>();
+        btnChangeName.onClick.AddListener(() => {
+            EventCenter.Broadcast<string>(EventType.ChangeName, "Hello Tom");
+        });
+    }
+
+}
---
+++ UI/EventSystem/Assets/Scripts/MainUI/UIPlayerName.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPlayerName : MonoBehaviour
+{
+    private Text playerName;
+
+    void Awake() {
+        playerName = GetComponent<Text>();
+
+        EventCenter.AddListener<string>(EventType.ChangeName, ChangeName);
+    }
+
+    void Destroy() {
+        EventCenter.RemoveListener<string>(EventType.ChangeName, ChangeName);
+    }
+
+    private void ChangeName(string newName) {
+        playerName.text = newName;
+    }
+}
